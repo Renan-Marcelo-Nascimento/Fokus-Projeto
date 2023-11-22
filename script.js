60a89a26dff861e9b0b1bdcaeba93473c98e1bff
Renan-Marcelo-Nascimento <renan-vermelho@hotmail.com>
2023-11-22 17:52:23 -0300

    refatorando metodos e funções, e usando um switch

--- script.js
+++ script.js
@@ -3,16 +3,41 @@ const html = document.querySelector('html');
 const botaoFoco = document.querySelector('.app__card-button--foco');
 const botaoCurto = document.querySelector('.app__card-button--curto');
 const botaoLongo = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 //cria um evento de clique para os botoes, no qual ao clicarmos muda o atributo da html
 botaoFoco.addEventListener('click', () =>{
-    html.setAttribute('data-contexto', 'foco')
+    alterarContexto('foco');
 });
 
 botaoCurto.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    alterarContexto('descanso-curto');
 });
 
 botaoLongo.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
+    alterarContexto('descanso-longo');
 });
+
+function alterarContexto(contexto){
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagens/${contexto}.png`)
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`;    
+
+            break;
+        case  'descanso-curto':
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = `Hora de voltar à superfície.<br>
+            <strong class="app__title-strong">Faça uma pausa longa!</strong>`; 
+        default:
+            break;
+    }
+
+}
