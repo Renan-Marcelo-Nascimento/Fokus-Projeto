4c240ade641e35b65e3eefe06639b8d3a59405b8
Renan-Marcelo-Nascimento <renan-vermelho@hotmail.com>
2023-11-21 23:32:00 -0300

    adicionando as 3 telas de fundo

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+//constantes que armazenam valores dos query selectors
 const html = document.querySelector('html');
 const botaoFoco = document.querySelector('.app__card-button--foco');
 const botaoCurto = document.querySelector('.app__card-button--curto');
+const botaoLongo = document.querySelector('.app__card-button--longo');
 
+//cria um evento de clique para os botoes, no qual ao clicarmos muda o atributo da html
 botaoFoco.addEventListener('click', () =>{
     html.setAttribute('data-contexto', 'foco')
 });
 
 botaoCurto.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
-})
+});
+
+botaoLongo.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
+});
