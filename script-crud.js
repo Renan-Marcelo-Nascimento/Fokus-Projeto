4ef86e2b01f7b3cbad11764c6ef7dd37af54fa7f
Renan-Marcelo-Nascimento <renan-vermelho@hotmail.com>
2024-01-04 18:53:22 -0300

    adicionando JSON stringfy

--- script-crud.js
+++ script-crud.js
@@ -4,11 +4,18 @@ const btn_addTarefa = document.querySelector('.app__button--add-task');
 const formAddTarefas = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
 
+const tarefas = []; 
 
 btn_addTarefa.addEventListener('click', ()=>{
     formAddTarefas.classList.toggle('hidden'); 
 })
 
 formAddTarefas.addEventListener('submit', (evento) =>{
-    //evento.preventDefault();
+    evento.preventDefault();
+    
+    const tarefa = {
+        descricao: textArea.value
+    } 
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
 })
