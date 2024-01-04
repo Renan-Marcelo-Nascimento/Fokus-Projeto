c0f8e9936d7d3a61376473ca10e7b0fa79a98406
Renan-Marcelo-Nascimento <renan-vermelho@hotmail.com>
2024-01-04 18:20:26 -0300

    adicionando prevent default no form

--- script-crud.js
+++ script-crud.js
@@ -2,8 +2,13 @@
 
 const btn_addTarefa = document.querySelector('.app__button--add-task');
 const formAddTarefas = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
 
 
 btn_addTarefa.addEventListener('click', ()=>{
     formAddTarefas.classList.toggle('hidden'); 
+})
+
+formAddTarefas.addEventListener('submit', (evento) =>{
+    //evento.preventDefault();
 })
