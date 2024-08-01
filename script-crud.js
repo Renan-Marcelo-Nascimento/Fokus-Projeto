6780d409066de339b2f08e46650b589650e0e7f9
Renan-Marcelo-Nascimento <renan-vermelho@hotmail.com>
2024-08-01 18:23:41 -0300

    adicionando clique para seleção e desseleção

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,7 @@ const formAddTarefas = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
+let tarefaSelecionada = null;
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []; 
 
@@ -52,7 +53,21 @@ function criarElementoTarefa(tarefa){
     li.append(paragrafo);
     li.append(botao);
     li.onclick = () =>{
+
+        document.querySelectorAll('.app__section-task-list-item-active')
+        .forEach(elemento => {
+            elemento.classList.remove('app__section-task-list-item-active');
+        })
+        
+        if(tarefaSelecionada == tarefa){
+           paragrafoDescricaoTarefa.textContent = '';
+           tarefaSelecionada = null;
+           return; 
+        }
+
+        tarefaSelecionada = tarefa;
         paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+        
         li.classList.add('app__section-task-list-item-active');
     }
 
