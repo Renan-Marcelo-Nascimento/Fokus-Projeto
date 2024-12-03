b2fab88ece6b188b8390b1b5a6cf1066b57ecb64
Renan-Marcelo-Nascimento <renan-vermelho@hotmail.com>
2024-12-02 22:38:31 -0300

    adicionando tarefa finalizada em verde

--- script-crud.js
+++ script-crud.js
@@ -6,6 +6,7 @@ const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
 let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []; 
 
@@ -62,10 +63,12 @@ function criarElementoTarefa(tarefa){
         if(tarefaSelecionada == tarefa){
            paragrafoDescricaoTarefa.textContent = '';
            tarefaSelecionada = null;
+           liTarefaSelecionada = null;
            return; 
         }
 
         tarefaSelecionada = tarefa;
+        liTarefaSelecionada = li;
         paragrafoDescricaoTarefa.textContent = tarefa.descricao;
         
         li.classList.add('app__section-task-list-item-active');
@@ -97,3 +100,11 @@ tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
 });
+
+document.addEventListener('FocoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada){
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active');
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete');
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled');
+    }
+})
